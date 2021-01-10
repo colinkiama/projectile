32199782a8dece539b6c4c6e4b4dc12832c22181
Colin Kiama <colinkiama@gmail.com>
2021-01-10 22:32:57 +0000

    build: :construction: Now ready to start generated html content

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import inlineCss from 'inline-css';
+var html = "<style>div{color:red;}</style><div/>";
+ 
+let options = {
+    url: "http://localhost:8080"
+}
+
+inlineCss(html, options)
+    .then(function(html) { console.log(html); });
