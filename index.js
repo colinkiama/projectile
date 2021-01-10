8fe42a32b3c0c7f7aa1f6bacf7eb12815d1be183
Colin Kiama <colinkiama@gmail.com>
2021-01-10 23:34:01 +0000

    feat: :sparkles: Now applies attributes to existing html content

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import inlineCss from 'inline-css';
-var html = "<style>div{color:red;}</style><div/>";
- 
-let options = {
-    url: "./"
+import fs from 'fs/promises';
+
+const TEMPLATE_FILE_PATH = "./src/index.html";
+const CSS_FILE_PATH = "./src/index.css";
+
+async function readFile(filePath) {
+    try {
+        const data = await fs.readFile(filePath);
+        return data.toString();
+    } catch (error) {
+        console.error(`Got an error trying to read the file: ${error.message}`);
+    }
 }
 
-inlineCss(html, options)
-    .then(function(html) { console.log(html); });
+(async function () {
+    let html = await readFile(TEMPLATE_FILE_PATH);
+    let css = await readFile(CSS_FILE_PATH);
+    // var html = "<style>div{color:red;}</style><div/>";
+
+    let options = {
+        url: "./",
+        extraCss: css
+    };
+
+    inlineCss(html, options)
+        .then(function (html) { console.log(html); });
+})();
